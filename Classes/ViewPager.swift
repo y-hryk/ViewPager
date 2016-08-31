05c2db240c68abc73710997374da7ca78c3572a4
y-hryk <dev.hy630823@gmail.com>
2016-08-31 11:15:21 +0900

    Initial commit

---
+++ Classes/ViewPager.swift
@@ -0,0 +1,13 @@
+//
+//  ViewPager.swift
+//  ViewPagerDemo
+//
+//  Created by yamaguchi on 2016/08/31.
+//  Copyright © 2016年 h.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewPager: UIViewController {
+
+}
